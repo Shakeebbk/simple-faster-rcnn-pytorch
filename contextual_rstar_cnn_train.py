676af7b8364de3bf7000d4737fb15f554f3ec2b2
Shakeeb Pasha <spasha@nvidia.com>
2021-05-06 17:20:42 +0530

    Dump result

--- contextual_rstar_cnn_train.py
+++ contextual_rstar_cnn_train.py
@@ -39,6 +39,18 @@ def eval(dataloader, rstar_cnn, test_num=10000):
         pred_scores += pred_scores_
         if ii == test_num: break
 
+    import pickle
+    result = {
+        "pred_bboxes" : pred_bboxes,
+        "pred_labels" : pred_labels, 
+        "pred_scores" : pred_scores,
+        "gt_bboxes"   : gt_bboxes,
+        "gt_labels"   : gt_labels
+    }
+
+    with open('results.pkl', 'wb') as f:
+        pickle.dump(result, f)
+
     result = eval_detection_voc(
         pred_bboxes, pred_labels, pred_scores,
         gt_bboxes, gt_labels, gt_difficults,
@@ -77,73 +89,78 @@ def train(**kwargs):
     if opt.load_path:
         trainer.load(opt.load_path)
         print('load pretrained model from %s' % opt.load_path)
-    #trainer.vis.text(dataset.db.label_names, win='labels')
-    print(dataset.db.label_names)
-    best_map = 0
-    lr_ = opt.lr
-    for epoch in range(opt.epoch):
-        trainer.reset_meters()
-        print(f"train its [{len(dataloader)}]")
-        for ii, (img, bbox_, label_, scale) in tqdm(enumerate(dataloader)):
-            scale = at.scalar(scale)
-            img, bbox, label = img.cuda().float(), bbox_.cuda(), label_.cuda()
-            trainer.train_step(img, bbox, label, scale)
 
-            # if (ii + 1) % opt.plot_every == 0:
-            #     if os.path.exists(opt.debug_file):
-            #         ipdb.set_trace()
+    print(f"val its [{len(val_dataloader)}]")
+    eval_result = eval(val_dataloader, rstar_cnn, test_num=opt.test_num)
+    print(eval_result)
 
-            #     # plot loss
-            #     trainer.vis.plot_many(trainer.get_meter_data())
+#     #trainer.vis.text(dataset.db.label_names, win='labels')
+#     print(dataset.db.label_names)
+#     best_map = 0
+#     lr_ = opt.lr
+#     for epoch in range(opt.epoch):
+#         trainer.reset_meters()
+#         print(f"train its [{len(dataloader)}]")
+#         for ii, (img, bbox_, label_, scale) in tqdm(enumerate(dataloader)):
+#             scale = at.scalar(scale)
+#             img, bbox, label = img.cuda().float(), bbox_.cuda(), label_.cuda()
+#             trainer.train_step(img, bbox, label, scale)
 
-            #     # plot groud truth bboxes
-            #     ori_img_ = inverse_normalize(at.tonumpy(img[0]))
-            #     gt_img = visdom_bbox(ori_img_,
-            #                          at.tonumpy(bbox_[0]),
-            #                          at.tonumpy(label_[0]))
-            #     trainer.vis.img('gt_img', gt_img)
+#             # if (ii + 1) % opt.plot_every == 0:
+#             #     if os.path.exists(opt.debug_file):
+#             #         ipdb.set_trace()
 
-            #     # plot predicti bboxes
-            #     _bboxes, _labels, _scores = trainer.rstar_cnn.predict([ori_img_], visualize=True)
-            #     pred_img = visdom_bbox(ori_img_,
-            #                            at.tonumpy(_bboxes[0]),
-            #                            at.tonumpy(_labels[0]).reshape(-1),
-            #                            at.tonumpy(_scores[0]))
-            #     trainer.vis.img('pred_img', pred_img)
+#             #     # plot loss
+#             #     trainer.vis.plot_many(trainer.get_meter_data())
 
-            #     # rpn confusion matrix(meter)
-            #     trainer.vis.text(str(trainer.rpn_cm.value().tolist()), win='rpn_cm')
-            #     # roi confusion matrix
-            #     trainer.vis.img('roi_cm', at.totensor(trainer.roi_cm.conf, False).float())
-#             log_info = 'lr:{}, loss:{}'.format(str(trainer.rstar_cnn.optimizer.param_groups[0]['lr']),
+#             #     # plot groud truth bboxes
+#             #     ori_img_ = inverse_normalize(at.tonumpy(img[0]))
+#             #     gt_img = visdom_bbox(ori_img_,
+#             #                          at.tonumpy(bbox_[0]),
+#             #                          at.tonumpy(label_[0]))
+#             #     trainer.vis.img('gt_img', gt_img)
+
+#             #     # plot predicti bboxes
+#             #     _bboxes, _labels, _scores = trainer.rstar_cnn.predict([ori_img_], visualize=True)
+#             #     pred_img = visdom_bbox(ori_img_,
+#             #                            at.tonumpy(_bboxes[0]),
+#             #                            at.tonumpy(_labels[0]).reshape(-1),
+#             #                            at.tonumpy(_scores[0]))
+#             #     trainer.vis.img('pred_img', pred_img)
+
+#             #     # rpn confusion matrix(meter)
+#             #     trainer.vis.text(str(trainer.rpn_cm.value().tolist()), win='rpn_cm')
+#             #     # roi confusion matrix
+#             #     trainer.vis.img('roi_cm', at.totensor(trainer.roi_cm.conf, False).float())
+# #             log_info = 'lr:{}, loss:{}'.format(str(trainer.rstar_cnn.optimizer.param_groups[0]['lr']),
+# #                                                       str(trainer.get_meter_data()))
+# #             print(log_info)
+#         print(f"val its [{len(val_dataloader)}]")
+#         eval_result = eval(val_dataloader, rstar_cnn, test_num=opt.test_num)
+#         # trainer.vis.plot('test_map', eval_result['map'])
+        
+#         # print(f"test its [{len(test_dataloader)}]")
+#         # test_eval_result = eval(test_dataloader, rstar_cnn, test_num=opt.test_num)
+        
+#         # trainer.vis.plot('test_map', eval_result['map'])
+#         lr_ = trainer.rstar_cnn.optimizer.param_groups[0]['lr']
+#         log_info = 'lr:{}, val_map:{}, loss:{}'.format(str(lr_),
+#                                                       str(eval_result['map']),
 #                                                       str(trainer.get_meter_data()))
-#             print(log_info)
-        print(f"val its [{len(val_dataloader)}]")
-        eval_result = eval(val_dataloader, rstar_cnn, test_num=opt.test_num)
-        # trainer.vis.plot('test_map', eval_result['map'])
-        
-        # print(f"test its [{len(test_dataloader)}]")
-        # test_eval_result = eval(test_dataloader, rstar_cnn, test_num=opt.test_num)
-        
-        # trainer.vis.plot('test_map', eval_result['map'])
-        lr_ = trainer.rstar_cnn.optimizer.param_groups[0]['lr']
-        log_info = 'lr:{}, val_map:{}, loss:{}'.format(str(lr_),
-                                                      str(eval_result['map']),
-                                                      str(trainer.get_meter_data()))
-        # trainer.vis.log(log_info)
-        print(log_info)
-        print(str(eval_result['ap']))
+#         # trainer.vis.log(log_info)
+#         print(log_info)
+#         print(str(eval_result['ap']))
 
-        # if eval_result['map'] > best_map:
-        best_map = eval_result['map']
-        best_path = trainer.save(best_map=best_map)
-        if epoch == 5:
-            trainer.load(best_path)
-            trainer.rstar_cnn.scale_lr(opt.lr_decay)
-            lr_ = lr_ * opt.lr_decay
+#         # if eval_result['map'] > best_map:
+#         best_map = eval_result['map']
+#         best_path = trainer.save(best_map=best_map)
+#         if epoch == 5:
+#             trainer.load(best_path)
+#             trainer.rstar_cnn.scale_lr(opt.lr_decay)
+#             lr_ = lr_ * opt.lr_decay
 
-        # if epoch == 13: 
-        #     break
+#         # if epoch == 13: 
+#         #     break
 
 
 if __name__ == '__main__':
